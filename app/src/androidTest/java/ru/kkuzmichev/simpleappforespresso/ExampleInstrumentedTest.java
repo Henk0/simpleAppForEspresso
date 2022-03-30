3f50a987246e74fc62646fa27839879d2d8a73da
Андрей Береснев <ihenk0@MacBook-Pro-Andrej-2.local>
2022-03-30 15:01:37 +0300

    Lab1 tests

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,14 +1,23 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +26,42 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+    }
+
+    // Negative
+    @Test
+    public void negativeCheckFragmentText() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+
+        mainText.check(
+                matches(
+                        withText("This is homie"))
+        );
+    }
+
+    // Positive
+    @Test
+    public void positiveCheckButtonIsDisplayed() {
+        ViewInteraction button = onView(
+                withId(R.id.fab)
+        );
+
+        button.perform(click())
+                .check(
+                        matches(
+                                isDisplayed())
+        );
     }
 }
