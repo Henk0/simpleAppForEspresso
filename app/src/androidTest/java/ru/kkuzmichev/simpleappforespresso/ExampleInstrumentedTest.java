db858056341679be71f0de8d6d0cdf09eb91fa1e
Андрей Береснев <ihenk0@MacBook-Pro-Andrej-2.local>
2022-03-30 16:30:39 +0300

    Lab2

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -6,7 +6,10 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.anyOf;
 
 import android.content.Context;
 
@@ -14,10 +17,25 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
+import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+
+import ru.kkuzmichev.simpleappforespresso.matchers.ItemVisibleMatcher;
+import ru.kkuzmichev.simpleappforespresso.matchers.SizeMatcher;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,6 +44,16 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
@@ -63,5 +91,34 @@ public class ExampleInstrumentedTest {
                         matches(
                                 isDisplayed())
         );
+    }
+
+    @Test
+    public void testIntent() {
+        Intents.init();
+
+        try {
+            openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        } catch (Exception ignored) {
+        }
+
+        onView(anyOf(withText(R.string.action_settings), withId(R.id.action_settings))).perform(click());
+        Intents.intended(hasData("https://google.com"));
+        Intents.release();
+    }
+
+    @Test
+    public void testGalleryList() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
+        onView(withId(R.id.recycle_view)).check(ViewAssertions.matches(SizeMatcher.of(10)));
+    }
+
+    @Test
+    public void testItemElementDisplayed() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
+        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
+        recyclerView.check(matches(new ItemVisibleMatcher(RecyclerView.class, 0)));
     }
 }
